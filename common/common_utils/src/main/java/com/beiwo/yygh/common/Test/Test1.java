0c6adb9bb9fdcd4a1db03485fe5fb2275163b390
root <3235486907@qq.com>
2022-02-10 20:25:53 +0800

    two commit

--- common/common_utils/src/main/java/com/beiwo/yygh/common/Test/Test1.java
+++
@@ -1,4 +0,0 @@
-package com.beiwo.yygh.common.Test;
-
-public class Test1 {
-}
--- common/common_utils/src/main/java/com/beiwo/yygh/common/result/Test.java
+++
@@ -1,7 +0,0 @@
-package com.beiwo.yygh.common.result;
-
-public class Test {
-    public static void main(String[] args) {
-
-    }
-}
